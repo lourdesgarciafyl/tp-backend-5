464654d2adfcc81b0b5fd4255bd622a9122c5bcd
cristianq3 <cristianq3@gmail.com>
2023-06-13 17:53:48 -0300

    Se agrega estructura de Login

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,15 @@ import EditarReceta from "./components/views/receta/EditarReceta";
 import Navegacion from "./components/common/Navbar";
 import Footer from "./components/common/Footer";
 import DetalleReceta from "./components/views/DetalleReceta";
+import Login from "./components/views/Login";
 
 function App() {
 
   return (
     <>
+    <Navegacion></Navegacion>
+    <Login></Login>
+ <Footer></Footer>
     </>
   )
 }
