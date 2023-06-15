ab74f47b7d2beca22e7fbcd77b84819a569f97b0
Maoi11 <marisaindianagarat@gmail.com>
2023-06-14 22:58:06 -0300

    se agregan archivos a assets y la imagen a pag inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css'
+import "./App.css";
+import Inicio from "./components/views/Inicio";
 import Administrador from "./components/views/Administrador";
 import CrearReceta from "./components/views/receta/CrearReceta";
 import EditarReceta from "./components/views/receta/EditarReceta";
 import Navegacion from "./components/common/Navbar";
 import Footer from "./components/common/Footer";
 
-
 function App() {
-
   return (
-    <>  
-    <Navegacion/>
-    <Footer/>
-    
+    <>
+      <Navegacion />
+      <Inicio></Inicio>
+      <Footer />
     </>
-  )
+  );
 }
-export default App
+export default App;
--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,11 +1,12 @@
 import {Container, Row} from "react-bootstrap";
 import CardReceta from "./receta/CardReceta";
+import banner from "../../assets/bannerPrueba.png"
 
 
 const Inicio = () => {
     return (
         <section className="mainSection">
-            <img className="bannerInicio">
+            <img className="bannerInicio" src={banner}>
             </img>
 
         </section>
