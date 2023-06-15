2116c8e13a469d1fb1ad4325537a1546516ee331
Maoi11 <marisaindianagarat@gmail.com>
2023-06-14 23:21:37 -0300

    se agregan imagenes, banner, se acomoda titulo en inicio

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,6 +1,6 @@
 import {Container, Row} from "react-bootstrap";
 import CardReceta from "./receta/CardReceta";
-import banner from "../../assets/bannerPrueba.png"
+import banner from "../../assets/banner2.jpeg"
 
 
 const Inicio = () => {
@@ -8,7 +8,17 @@ const Inicio = () => {
         <section className="mainSection">
             <img className="bannerInicio" src={banner}>
             </img>
-
+            <Container>
+                <h1 className="display-4 text-center my-2 letraDestacada">Rolling Flavors</h1>
+                <p className="text-center my-3"><em>¡Encontrá aquí las mejores recetas para planificar tu semana!</em></p>
+                <hr />
+                <Row>
+                    <CardReceta></CardReceta>
+                    <CardReceta></CardReceta>
+                    <CardReceta></CardReceta>
+                    <CardReceta></CardReceta>
+                </Row>
+            </Container>
         </section>
     );
 };
--- src/components/views/receta/CardReceta.jsx
+++ src/components/views/receta/CardReceta.jsx
@@ -1,10 +1,11 @@
 import { Col, Card, Badge } from "react-bootstrap";
+import pastel from "../../../assets/pasteldepapas.jpg" //después saco esta imagen
 
 const CardReceta = () => {
   return (
     <Col md={4} ld={3} className="mb-3">
       <Card>
-      <Card.Img variant="top" src=""></Card.Img>
+      <Card.Img variant="top" src={pastel}></Card.Img>
         <Card.Body>
           <Card.Title>Pastel de Papas</Card.Title>
           <div className="d-flex justify-content-evenly">
