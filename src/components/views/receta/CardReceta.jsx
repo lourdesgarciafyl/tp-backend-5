9d5c3bc5dd411e88c8a2a751adcce88d91106702
Maoi11 <marisaindianagarat@gmail.com>
2023-06-14 21:36:34 -0300

    se agrega estructura basica del componente

---
+++ src/components/views/receta/CardReceta.jsx
@@ -0,0 +1,14 @@
+import { Col, Card, Badge } from "react-bootstrap";
+
+const CardReceta = () => {
+  return (
+    <Col md={4} ld={3} className="mb-3">
+      <Card>
+        
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+
+export default CardReceta;
