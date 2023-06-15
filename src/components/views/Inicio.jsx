a6a713011a5f1af3c63ac5e3d5ba89cbca9e9e6c
Maoi11 <marisaindianagarat@gmail.com>
2023-06-14 22:43:25 -0300

    estructura de inicio

---
+++ src/components/views/Inicio.jsx
@@ -0,0 +1,15 @@
+import {Container, Row} from "react-bootstrap";
+import CardReceta from "./receta/CardReceta";
+
+
+const Inicio = () => {
+    return (
+        <section className="mainSection">
+            <img className="bannerInicio">
+            </img>
+
+        </section>
+    );
+};
+
+export default Inicio;
