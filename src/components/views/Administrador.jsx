db6234a3bce9e357944c1405f7b3f57096d775ae
cristianq3 <cristianq3@gmail.com>
2023-06-18 09:57:49 -0300

    Se agrega el read de recetas, se crea itemReceta con los props recibidos.

--- src/components/views/Administrador.jsx
+++ src/components/views/Administrador.jsx
@@ -3,47 +3,45 @@ import ItemReceta from "./receta/ItemReceta";
 import { useEffect, useState } from "react";
 import { consultaListaRecetas } from "../helpers/queries";
 
+const Administrador = () => {
+  const [recetas, setRecetas] = useState([]);
 
-const Administrador = () =>{
-const [recetas, setRecetas] = useState([]);
+  useEffect(() => {
+    consultaListaRecetas().then((respuesta) => {
+      console.log(respuesta);
 
-useEffect(()=>{
+      setRecetas(respuesta);
+    });
+  }, []);
 
-  consultaListaRecetas().then((respuesta)=>{
-    console.log(respuesta);
+  return (
+    <section className="container mainSection">
+      <h1 className="mt-2 text-center tituloDestacado letraDestacada">
+        Administracion de recetas
+      </h1>
+      <div className="d-flex justify-content-center mt-1">
+        <Button id="btnCrearReceta">Agregar</Button>
+      </div>
+      <hr />
+      <Table responsive striped bordered hover>
+        <thead>
+          <tr className="text-center">
+            <th>Codigo</th>
+            <th>Receta</th>
+            <th>Dificultad</th>
+            <th>Tiempo</th>
+            <th>Ingredientes</th>
+            <th>URL de Imagen</th>
+            <th>Opciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {recetas.map((receta)=>  <ItemReceta key={receta.id} receta={receta}></ItemReceta> )}
+         
+        </tbody>
+      </Table>
+    </section>
+  );
+};
 
-    setRecetas(respuesta);
-
-  })
-
-}, [])
-
-
-    return(
-        <section className="container mainSection">
-            <h1 className="mt-2 text-center tituloDestacado letraDestacada">Administracion de recetas</h1>
-            <div className="d-flex justify-content-center mt-1">
-                <Button id="btnCrearReceta">Agregar</Button>
-            </div>
-            <hr />
-            <Table responsive striped bordered hover>
-          <thead>
-            <tr className="text-center">
-              <th>Codigo</th>
-              <th>Receta</th>
-              <th>Dificultad</th>
-              <th>Tiempo</th>
-              <th>Ingredientes</th>
-              <th>URL de Imagen</th>
-              <th>Opciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            <ItemReceta></ItemReceta>
-          </tbody>
-        </Table>
-        </section>
-    )
-}
-
-export default Administrador
+export default Administrador;
--- src/components/views/receta/ItemReceta.jsx
+++ src/components/views/receta/ItemReceta.jsx
@@ -1,14 +1,14 @@
 import { Button } from "react-bootstrap";
 
-const ItemReceta = () =>{
+const ItemReceta = ({receta}) =>{
     return(
         <tr>
-            <td className="text-truncate"># 1</td>
-            <td className="text-truncate">Budín de zanahoria</td>
-            <td className="text-truncate">Dificil</td>
-            <td className="text-truncate">80 minutos</td>
-            <td className="tamanioCelda text-truncate">Ingredientes</td>
-            <td className="tamanioCelda text-truncate">https://www.paulinacocina.net/wp-content/uploads/2020/04/budin-de-zanahoria-800x558.jpg</td>
+            <td className="text-truncate">{receta.id}</td>
+            <td className="text-truncate">{receta.nombre}</td>
+            <td className="text-truncate">{receta.dificultad}</td>
+            <td className="text-truncate">{receta.tiempo}</td>
+            <td className="tamanioCelda text-truncate">{receta.ingredientes}</td>
+            <td className="tamanioCelda text-truncate">{receta.imagen}</td>
             <td className="text-center">
              <Button className="btnEditar mb-2 mb-lg-0 me-md-1">
                 Editar </Button>
