f9c4356d4d059fdc943fa5b21c38fc156098dc1e
cristianq3 <cristianq3@gmail.com>
2023-06-18 11:07:47 -0300

    Se realiza el delete

--- src/components/views/Administrador.jsx
+++ src/components/views/Administrador.jsx
@@ -36,7 +36,7 @@ const Administrador = () => {
           </tr>
         </thead>
         <tbody>
-          {recetas.map((receta)=>  <ItemReceta key={receta.id} receta={receta}></ItemReceta> )}
+          {recetas.map((receta)=>  <ItemReceta key={receta.id} receta={receta} setRecetas={setRecetas}></ItemReceta> )}
          
         </tbody>
       </Table>
--- src/components/views/receta/ItemReceta.jsx
+++ src/components/views/receta/ItemReceta.jsx
@@ -1,45 +1,59 @@
 import { Button } from "react-bootstrap";
 import Swal from "sweetalert2";
+import {
+  consultaBorrarReceta,
+  consultaListaRecetas,
+} from "../../helpers/queries";
 
-const ItemReceta = ({receta}) =>{
+const ItemReceta = ({ receta, setRecetas }) => {
+  const borrarReceta = () => {
+    Swal.fire({
+      title: "Esta seguro de borrar la receta?",
+      text: "No se puede revertir este paso!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Borrar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        consultaBorrarReceta(receta.id).then(() => {
+          console.log(respuesta);
+        });
+        Swal.fire("Receta eliminada!", "Su receta fue eliminada.", "success");
 
-    const borrarReceta = ()=>{
-        Swal.fire({
-            title: 'Esta seguro de borrar la receta?',
-            text: "No se puede revertir este paso!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Borrar',
-            cancelButtonText: 'Cancelar',
-          }).then((result) => {
-            if (result.isConfirmed) {
-              Swal.fire(
-                'Receta eliminada!',
-                'Su receta fue eliminada.',
-                'success'
-              )
-            }
-          })
+        consultaListaRecetas().then((respuesta)=> setRecetas(respuesta))
 
-    }
-    return(
-        <tr>
-            <td className="text-truncate">{receta.id}</td>
-            <td className="text-truncate">{receta.nombre}</td>
-            <td className="text-truncate">{receta.dificultad}</td>
-            <td className="text-truncate">{receta.tiempo}</td>
-            <td className="tamanioCelda text-truncate">{receta.ingredientes}</td>
-            <td className="tamanioCelda text-truncate">{receta.imagen}</td>
-            <td className="text-center">
-             <Button className="btnEditar mb-2 mb-lg-0 me-md-1">
-                Editar </Button>
-             <Button className="btnEliminar" onClick={borrarReceta}>
-              Borrar </Button>
+   
+      } else {
+
+        Swal.fire(
+            'Ocurrio un error',
+            `Intente realizar esta operación nuevamente mas tarde`,
+            'success'
+          )
+        }
+        
+      
+    });
+  };
+  return (
+    <tr>
+      <td className="text-truncate">{receta.id}</td>
+      <td className="text-truncate">{receta.nombre}</td>
+      <td className="text-truncate">{receta.dificultad}</td>
+      <td className="text-truncate">{receta.tiempo}</td>
+      <td className="tamanioCelda text-truncate">{receta.ingredientes}</td>
+      <td className="tamanioCelda text-truncate">{receta.imagen}</td>
+      <td className="text-center">
+        <Button className="btnEditar mb-2 mb-lg-0 me-md-1">Editar </Button>
+        <Button className="btnEliminar" onClick={borrarReceta}>
+          Borrar{" "}
+        </Button>
       </td>
-        </tr>
-    )
-}
+    </tr>
+  );
+};
 
 export default ItemReceta;
