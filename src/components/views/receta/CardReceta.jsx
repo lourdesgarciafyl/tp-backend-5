71eec5024989d799277204a3ed38ec4961186814
cristianq3 <cristianq3@gmail.com>
2023-06-18 22:58:07 -0300

    Se realiza el read de la pagina de inicio para mostrar las cards.

--- src/components/views/receta/CardReceta.jsx
+++ src/components/views/receta/CardReceta.jsx
@@ -1,25 +1,32 @@
 import { Col, Card, Badge, Button } from "react-bootstrap";
 import pastel from "../../../assets/pasteldepapas.jpg"; //después saco esta imagen
+import { consultaListaRecetas} from "../../helpers/queries";
 
-const CardReceta = () => {
+
+
+const CardReceta = ({receta}) => {
+  
+
+
+
   return (
     <Col md={4} ld={3} className="mb-3">
       <Card className="cardsInicio">
         <Card.Img variant="top" src={pastel}></Card.Img>
         <Card.Body>
           <Card.Title className="text-center letraDestacada">
-            Pastel de Papas
+            {receta.nombre}
           </Card.Title>
           <hr />
           <div className="d-flex justify-content-evenly">
             <ul>
               <li>
                 Dificultad:{" "}
-                <Badge className="badgeInicio bg-light">Media</Badge>
+                <Badge className="badgeInicio bg-light">{receta.dificultad}</Badge>
               </li>
               <li>
                 Tiempo de preparación:{" "}
-                <Badge className="badgeInicio bg-light">60 min</Badge>
+                <Badge className="badgeInicio bg-light">{receta.tiempo} min</Badge>
               </li>
             </ul>
           </div>
